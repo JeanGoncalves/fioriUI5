fd2ed7f8faf31c9cfb8a1e50b567bcb499b04cc1
Jean Gonçalves <jeanfpgoncalves@live.com>
2017-02-12 16:15:08 -0200

    initial structure [view, controller]

---
+++ controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend('sap.ui.fiori.controller.App', {
+
+		onInit: function() {
+			console.info('App.Controler.js');
+		}
+
+	});
+});
