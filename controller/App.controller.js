287ba351a573582ac55bc16cfb7be4182934b513
Jean Gonçalves <jeanfpgoncalves@live.com>
2017-02-12 16:49:41 -0200

    inserindo mais arquivos para estruturamento

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,12 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/resource/ResourceModel"
+
+
+], function(Controller, ResourceModel) {
 	"use strict";
 
 	return Controller.extend('sap.ui.fiori.controller.App', {
 
 		onInit: function() {
 			console.info('App.Controler.js');
+
 		}
 
 	});
